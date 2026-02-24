4285fb82a6af208b6cec834350e08e2b3a5092a0
dev rai <devdavinchi06@gmail.com>
2026-02-24 15:17:37 +0545

    feat: implement navigation bar using stateful widget - learned about stateless and stateful - learned the trigger mechanism setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HomePage());
 }
 
-String? title;
+// stateful can refresh
+// stateless can't refresh
+// setState to refresh
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+String title = "Dev's App";
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int change = 0;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.blue,
-            brightness: Brightness.dark,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black12,
+          brightness: Brightness.dark,
+        ),
+      ),
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title, style: TextStyle(color: Colors.black)),
+          centerTitle: false,
+
+          actions: [
+            Text("back", style: TextStyle(color: Colors.black)),
+            Icon(Icons.login_rounded, color: Colors.black),
+          ],
+          backgroundColor: Colors.grey,
+        ),
+
+        body: change == 0
+            ? Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(children: [Text("dev")]),
+                      // SizedBox(width: 10.10),
+                      Column(children: [Text("namita")]),
+                    ],
+                  ),
+
+                  Row(
+                    children: [
+                      Column(children: [Text("dev")]),
+                      SizedBox(width: 10.10),
+                      Column(children: [Text("namita")]),
+                    ],
+                  ),
+                ],
+              )
+            : Center(child: Text("namita")),
+        drawer: Drawer(
+          backgroundColor: Color(0xFFA9A9A9),
+          child: Column(
+            children: [
+              // DrawerHeader(child: Text("head")),
+              ListTile(title: Text("mario")),
+            ],
           ),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(title!, style: TextStyle(color: Colors.black)),
-            centerTitle: false,
-
-            actions: [
-              Text("back", style: TextStyle(color: Colors.black)),
-              Icon(Icons.login_rounded, color: Colors.black),
-            ],
-            backgroundColor: Colors.grey,
-          ),
-          body: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(children: [Text("dev")]),
-                  // SizedBox(width: 10.10),
-                  Column(children: [Text("namita")]),
-                ],
-              ),
-
-              Row(
-                children: [
-                  Column(children: [Text("dev")]),
-                  SizedBox(width: 10.10),
-                  Column(children: [Text("namita")]),
-                ],
-              ),
-            ],
-          ),
-          drawer: Drawer(
-            backgroundColor: Color(0xFFA9A9A9),
-            child: Column(
-              children: [
-                // DrawerHeader(child: Text("head")),
-                ListTile(title: Text("mario")),
-              ],
-            ),
-          ),
-          bottomNavigationBar: NavigationBar(
-            destinations: [
-              NavigationDestination(icon: Icon(Icons.home), label: "home"),
-              NavigationDestination(icon: Icon(Icons.people), label: "people"),
-            ],
-          ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: "home"),
+            NavigationDestination(icon: Icon(Icons.people), label: "people"),
+          ],
+          onDestinationSelected: (int value) {
+            setState(() {
+              change = value;
+            });
+          },
+          selectedIndex: change,
         ),
       ),
     );
