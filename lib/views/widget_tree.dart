4caac9d933100839c49af4997b08a5aea01dd457
dev rai <devdavinchi06@gmail.com>
2026-03-12 15:46:49 +0545

    feat(ui) : add setting icon with its navigation

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,6 +1,7 @@
 import 'package:first_flutter_app/data/notifiers.dart';
 import 'package:first_flutter_app/views/pages/home_page.dart';
 import 'package:first_flutter_app/views/pages/profile_page.dart';
+import 'package:first_flutter_app/views/pages/setting_page.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/navi_bar.dart';
@@ -27,6 +28,19 @@ class WidgetTree extends StatelessWidget {
                 return Icon(themeChan ? Icons.dark_mode : Icons.light_mode);
               },
             ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SettingPage();
+                  },
+                ),
+              );
+            },
+            icon: Icon(Icons.settings),
           ),
         ],
         backgroundColor: Colors.grey,
